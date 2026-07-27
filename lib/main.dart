89402a97502e790b50b684660d9b9361d5ad73ed
Cypher Allan <cypher.allan@gmail.com>
2026-07-27 13:42:45 +0300

    Integrate Firebase and initialize application

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'injection_container.dart' as di;
+
 import 'features/home/presentation/pages/home_page.dart';
+import 'firebase_options.dart';
+import 'injection_container.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await di.init();
+
   runApp(const MyApp());
 }
 
@@ -17,7 +26,9 @@ class MyApp extends StatelessWidget {
       title: 'Chat Story Generator',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.green,
+        ),
         useMaterial3: true,
       ),
       home: const HomePage(),
